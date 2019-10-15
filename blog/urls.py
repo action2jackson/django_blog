5d10268af17d2efd1487cdaf878ce2ddbdac3ee9
Jackson <2action2jackson2@gmail.com>
2019-10-14 22:43:38 -0500

    Add detail_view for every blog post

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # 127.0.0.1:8000 --> Local
+    # mydjangosite.com --> Online
     # If you go to an empty location take us to views.post_list
     path('', views.post_list, name='post_list'),
+
+    # 127.0.0.1:8000/post/2 == You will be taken to the post that has a primary key of 2
+    # <int == You need to put in a number
+    path('post/<int:pk>/', views.post_detail, name='post_detail')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
 
@@ -9,3 +9,9 @@ def post_list(request):
     stuff_for_frontend = {'posts': posts}
     # Requesting access to the html file post_list
     return render(request, 'blog/post_list.html', stuff_for_frontend)
+
+def post_detail(request, pk):
+    # pk == primary key
+    post = get_object_or_404(Post, pk=pk)
+    stuff_for_frontend = {'post': post}
+    return render(request, 'blog/post_detail.html', stuff_for_frontend)
