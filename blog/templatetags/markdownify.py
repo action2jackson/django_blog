6ab1e0c9867645b3920b4422f90160ea8df8c214
Jackson <2action2jackson2@gmail.com>
2019-11-07 23:30:46 -0600

    Add markdown and syntax highlighting to blog posts and comments

---
+++ blog/templatetags/markdownify.py
@@ -0,0 +1,26 @@
+from django import template
+# Had to pip install markdown, mistune and pygments
+import mistune
+from pygments import highlight
+from pygments.lexers import get_lexer_by_name
+from pygments.formatters.html import HtmlFormatter
+
+register = template.Library()
+
+
+class HighlightRenderer(mistune.Renderer):
+    def block_code(self, code, lang):
+        if not lang:
+            # if text is wrapped like this it sends it to the zenburn css file and beautifies it.
+            return '\n<pre><code>%s</code></pre>\n' % \
+                   mistune.escape(code)
+        lexer = get_lexer_by_name(lang, stripall=True)
+        formatter = HtmlFormatter()
+        return highlight(code, lexer, formatter)
+
+
+@register.filter
+def markdown(value):
+    renderer = HighlightRenderer()
+    markdown = mistune.Markdown(renderer=renderer)
+    return markdown(value)
