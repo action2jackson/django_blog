6536f1b30d59ab877636968a9cb65737f0a95b66
Jackson <2action2jackson2@gmail.com>
2019-11-12 20:09:09 -0600

    Made the frontend look better

--- blog/views.py
+++ blog/views.py
@@ -89,7 +89,6 @@ def post_delete(request, pk):
     post.delete()
     return redirect('/', pk=post.pk)
 
-@login_required
 def add_comment_to_post(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == 'POST':
